d189487c19885f54ce9a17de49f3736c8aec2a31
Tknott95 <tknott95@hotmail.com>
2018-01-12 03:22:35 -0800

    Walt Tex Mixed Reality - @TK

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// if the Fire1 button (default is Ctrl) is pressed.
+public class PlayerFire : MonoBehaviour {
+    public GameObject projectile;
+    public float fireDelta = 0.5F;
+    public float shotSpeed = 50;
+
+    public float ammo = 20;
+    public Text ammoCount;
+
+    private float nextFire = 0.5F;
+    private GameObject newProjectile;
+    private float myTime = 0.0F;
+
+    private void Awake()
+    {
+        ammoCount.text = ammo.ToString();
+    }
+
+    void Update()
+    {
+        myTime = myTime + Time.deltaTime;
+
+        while (ammo > 0)
+        {
+            if (Input.GetButton("Fire1") && myTime > nextFire)
+            {
+                nextFire = myTime + fireDelta;
+                newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+                newProjectile.GetComponent<Rigidbody>().velocity = newProjectile.transform.forward * shotSpeed;
+                ammo--; ;
+                ammoCount.text = ammo.ToString();
+                // create code here that animates the newProjectile
+
+                nextFire = nextFire - myTime;
+                myTime = 0.0F;
+
+                // Destroy the bullet after 2 seconds
+                Destroy(newProjectile, 2.0f);
+            }
+        }
+    }
+}
+
